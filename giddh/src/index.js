1e0fe58b794fb483216b4653d63f0638f0e77b8b
ABIR PAUL <65394557+wandererabir@users.noreply.github.com>
2021-12-25 15:23:57 +0530

    Login Auth0 :+1:

--- giddh/src/index.js
+++ giddh/src/index.js
@@ -11,11 +11,9 @@ ReactDOM.render(
     domain={domain}
     clientId={clientId}
     redirectUri={window.location.origin}>
+    <App />
   </Auth0Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
