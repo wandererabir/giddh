1c89526833f835e14a12a150cf598995d05f934c
ABIR PAUL <65394557+wandererabir@users.noreply.github.com>
2021-12-26 09:36:47 +0530

    Logout done

--- giddh/src/App.js
+++ giddh/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import Logout from './components/Logout';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <Login />
+    <>
+      <Login />
+      <Logout />
+    </>
+
   );
 }
 
---
+++ giddh/src/components/Logout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+
+
+const Logout = () => {
+  const { logout, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <button onClick={() => logout({returnTo: "/Login"})}>
+        Log Out
+      </button>
+    )
+  )
+}
+
+export default Logout
