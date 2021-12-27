24cccdbba6e909292db18ad10e0f2ac0e51fcc11
ABIR PAUL <65394557+wandererabir@users.noreply.github.com>
2021-12-27 14:11:03 +0530

    login page done

--- giddh/src/components/Login.js
+++ giddh/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth0  } from '@auth0/auth0-react';
-import {Button, Image} from 'react-bootstrap';
+import {Button,Card} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const Login = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -8,10 +8,20 @@ const Login = () => {
     return (
       !isAuthenticated && (
         <>
-        <div className="container-fluid bg-dark" style={{height: "100vh"}}>
-        <Button style={{position:"absolute", top: "50%", left: "50%", transform: "translateY(-50%)"}} onClick={() => loginWithRedirect()}>
-          Log In
-        </Button>
+        <div className="container-fluid bg-dark  row align-items-center" style={{height: "100vh"}}>
+        <Card className="bg-primary text-white mx-auto" style={{width:"28rem"}} >
+          <Card.Header as="h5">Welcome To GIDDH</Card.Header>
+          <Card.Body>
+             <Card.Title>Are you member or not? Then register with us</Card.Title>
+             <Card.Text>
+               Click on it to Login/SignUp
+             </Card.Text>
+             <Button className='bg-white text-dark' onClick={() => loginWithRedirect()}>
+               Log In
+             </Button>
+           </Card.Body>
+        </Card>
+
         </div>
         </>
 
