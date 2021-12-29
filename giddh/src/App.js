68104b4e09fc64b68c7941cdcb94b730756d7cf0
aman-shri <shrivastava250@gmail.com>
2021-12-29 21:29:57 +0530

    Added the Database Form
    Added the bootstrap components and created the input page for the database.

--- giddh/src/App.js
+++ giddh/src/App.js
@@ -5,6 +5,7 @@ import Profile from './components/Profile';
 
 import { useAuth0 } from '@auth0/auth0-react';
 import { Route, Routes } from 'react-router-dom';
+import Database from './components/Database';
 
 function App() {
   const { isLoading } = useAuth0();
@@ -19,6 +20,12 @@ function App() {
              <Route exact path='/' component={Login}/> 
              <Route path='/features' component={Login}/>            
       </Routes>
+
+      <div className='row'>
+        <div className='col-md-8 offset-md-1'>
+          <Database />
+        </div>
+      </div>
     </>
 
   );
