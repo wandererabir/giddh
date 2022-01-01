8c4a1adbb86786ed0dcf437911f1748ccaf1df1b
ABIR PAUL <65394557+wandererabir@users.noreply.github.com>
2022-01-01 16:53:24 +0530

    small changes

--- src/views/Database.js
+++ src/views/Database.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { JSON_API } from '../utils/Constants';
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
